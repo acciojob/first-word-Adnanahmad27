779e64dde513c94e88d1df3a25ceae364b76d630
Adnan Ahmad <98473885+Adnanahmad27@users.noreply.github.com>
2024-01-12 15:24:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let index = 0;
+	for(let i=0 ; i<s.length ; i++){
+		if(s[i]===" "){
+			index = i;
+			break;
+		}
+	}
+	return s.slice(0,index);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
